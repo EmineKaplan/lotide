9efa707f2dfba732980bee92e09d676240070289
Emine Kaplan <eminekaplantr@gmail.com>
2020-11-27 01:25:02 +0000

    Our map function

---
+++ map.js
@@ -0,0 +1,31 @@
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) return false;
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+  return true;
+};
+const assertArraysEqual = function(array1, array2) {
+  if (!eqArrays(array1, array2)) return console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
+  return console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+};
+
+
+const map = function(array,callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+ 
+}
+const words1 = ["Ali","Veli","Deli"];
+const words2 =["ground", "control", "to", "major", "tom"];
+const words3 = ["ground", "control", "to", "major", "bob"];
+const numbers = [1,5,10,15,23];
+
+assertArraysEqual(map(words1, word => word[1]), [ "l", "e","e"]);
+assertArraysEqual(map(words2, word => word[1]), [ 'g', 'c', 't', 'm', 't' ]); 
+assertArraysEqual(map(words3, word => word[1]), [ 'g', 'c', 't', 'm', 't' ]); 
+assertArraysEqual(map(numbers, num => num*3), [ 3, 15, 30, 45, 69]); 
